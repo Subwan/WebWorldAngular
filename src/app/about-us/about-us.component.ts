b103bac4080ccad7374c9c859c1dff16d6aae803
alexandr <Subwan@gmail.com>
2017-12-05 13:41:59 +0300

    Add map and marker.

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var google: any;
+
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.css']
 })
+
 export class AboutUsComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+    var building = {lat: 51.667242, lng: 39.195113};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: building,
+      zoom: 15
+    });
+    var marker = new google.maps.Marker({
+         position: building,
+         map: map
+       });
   }
 
 }
